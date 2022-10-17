799aed3514509e8784865d8511774bb9548c72e3
melony <hjz1010@gmail.com>
2022-10-17 20:53:01 +0900

    Nest.js 학습 ~CRUD without DB

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+    constructor(private boardsService: BoardsService) {}
+
+    @Get() // 4. 사용할 메소드를 데코레이터로 달아준다.
+    getAllBoard(): Board[]  {
+                //7. ↑ 모델에 정의해둔 형태가 들어오도록 타입을 지정해준다.
+        return this.boardsService.getAllBoards();
+    }
+
+    @Post()   // 10. 게시물 생성에 사용할 controller
+    createBoard(
+        @Body() createBoardDto: CreateBoardDto  //12. controller에 DTO 적용하기 
+    ): Board {
+        return this.boardsService.createBoard(createBoardDto)
+    }
+    //15. controller
+    @Get('/:id')  // ↓ path parameter에서 키가 id인 값을 가져와서 
+    getBoardById(@Param('id') id: string): Board {
+                            // ↑ id라는 변수에 담는다!
+        return this.boardsService.getBoardById(id)
+    }
+    //17.
+    @Delete('/:id')
+    deleteBoard(@Param('id') id:string): void {
+        this.boardsService.deleteBoard(id)
+    }
+    //19.
+    @Patch('/:id/status')  // path parameter뒤에 추가 경로 덧붙이기!!
+    updateBoardStatus(
+        @Param('id') id:string,
+        @Body('status') status: BoardStatus
+    ) {
+        return this.updateBoardStatus(id, status);
+    }
+
+}
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    private boards: Board[] = []; //1. private를 사용하여 다른 컴포넌트에서 이 배열값을 수정할 수 없도록
+                //7. ↑ ↓ 모델에 정의해둔 형태가 들어오도록 타입을 지정해준다.
+    getAllBoards(): Board[] {  //2. 메소드 만들기
+        return this.boards;  //3. DB 대신 사용하는 배열 반환
+    }
+
+    createBoard(createBoardDto: CreateBoardDto) { //8. 게시물 생성 API 
+                  //13. ↑ ↓ service에 DTO 적용
+        const { title, description } = createBoardDto;
+        const board: Board = {
+            id: uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC
+        }
+
+        this.boards.push(board); //9. 배열에 새 게시물 넣기
+        return board;  // 새로 생성된 게시물 반환
+    }
+    //14. service
+    getBoardById(id: string): Board { // 게시글 하나 반환하므로 []없이
+        return this.boards.find((board) => board.id === id) // 배열에서 id가 일치하는 게시물 찾기
+    }
+    //16.
+    deleteBoard(id: string): void { //void: 반환값이 없으므로
+        this.boards = this.boards.filter((board) => board.id !== id);
+    }
+    //18. 
+    updateBoardStatus(id: string, status: BoardStatus): Board {
+        const board = this.getBoardById(id);
+        board.status = status;
+        return board;
+    }
+}
