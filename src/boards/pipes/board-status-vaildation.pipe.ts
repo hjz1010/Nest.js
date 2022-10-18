6a4b58d3a6d0062d678906f3d2e74751b6345fb0
melony <hjz1010@gmail.com>
2022-10-19 00:11:24 +0900

    Nest.js 학습 ~PIPE로 유효성체크하기(추가)

--- src/boards/pipes/board-status-vaildation.pipe.ts
+++ src/boards/pipes/board-status-vaildation.pipe.ts
@@ -1,10 +1,26 @@
-import { ArgumentMetadata, PipeTransform } from "@nestjs/common";
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { BoardStatus } from "../board.model";
 //24.  
 export class BoardStatusValidationPipe implements PipeTransform {
-    transform(value: any, metadata: ArgumentMetadata) {
-        console.log('value', value)
-        console.log('metadata',metadata)
+    readonly StatusOptions = [ //25. 접두사 readOnly 를 사용해서 읽기전용 값을 만들자
+        BoardStatus.PRIVATE,
+        BoardStatus.PUBLIC
+    ]
 
+    transform(value: any, metadata: ArgumentMetadata) {
+        // console.log('value', value) 
+        // console.log('metadata',metadata)
+        value = value.toUpperCase();  //26. 들어온 값을 대문자로 바꾸고 
+
+        if (!this.isStatusVaild(value)) { //27. 함수를 정의해서 사용하자 
+            throw new BadRequestException(`${value} is NOT in the status options`)
+        }
         return value;
     }
+
+    private isStatusVaild(status: any) {  //28. status 를 확인하는 함수 정의
+        const index = this.StatusOptions.indexOf(status);
+        return index !== -1
+    }
 }
+
